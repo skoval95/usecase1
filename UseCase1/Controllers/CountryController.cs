13e1d77c97603ed0531379a23a070504f0180fcd
SOFTSERVE\sk <sk@softserveinc.com>
2023-06-13 14:46:08 +0300

    UC#1 Infrastructure setup

---
+++ UseCase1/Controllers/CountryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UseCase1.Controllers
+{
+    public class CountryController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
